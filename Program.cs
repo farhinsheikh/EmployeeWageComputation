679a8a3801ce1bcafaa0c3bc97d23e004d6657f6
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-13 00:33:53 +0530

    [Farhin] ADDED: Random function to check employee attendence

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+          Console.WriteLine("Welcome to Employee Wage Computation Problem");
+           
+            int PRESENT = 1;
+            Random random = new Random();
+            int EmpCheack = random.Next(0, 2);
+            if (EmpCheack == 1)
+            {
+             Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+             Console.WriteLine("Employe Is Absent");
+            }
+
+            
         }
     }
 }
