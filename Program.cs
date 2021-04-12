4e4651eba2019ff046e310c0de49847b661c3abe
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-13 01:34:40 +0530

    [Farhin]ADDED:Part time employee & wage

--- Program.cs
+++ Program.cs
@@ -8,16 +8,21 @@ namespace EmployeeWageComputation
         {
           Console.WriteLine("Welcome to Employee Wage Computation Problem");
 
-            int PRESENT = 1;
+            int FULL_TIME = 1;
+            int PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int emphrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int EmpCheack = random.Next(0, 2);
-            if (EmpCheack == PRESENT)
+            int EmpCheack = random.Next(0, 3);
+            if (EmpCheack == FULL_TIME)
             {
                 emphrs = 8;
 
+            }
+            else if (EmpCheack == PART_TIME)
+            {
+                emphrs = 4;
             }
             else
             {
