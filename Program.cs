45d797e74c717c360f7034964efa4723ab3781db
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-13 01:07:39 +0530

    [Farhin] Calculate Dialy Employee Wage

--- Program.cs
+++ Program.cs
@@ -7,18 +7,24 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
           Console.WriteLine("Welcome to Employee Wage Computation Problem");
-           
+
             int PRESENT = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int EmpCheack = random.Next(0, 2);
-            if (EmpCheack == 1)
+            if (EmpCheack == PRESENT)
             {
-             Console.WriteLine("Employee Is Present");
+                emphrs = 8;
+
             }
             else
             {
-             Console.WriteLine("Employe Is Absent");
+                emphrs = 0;
             }
+            empWage = EMP_RATE_PER_HOUR * emphrs;
+            Console.WriteLine("Employe Wage Per Day:- " + empWage);
         }
     }
 }
